565a1422963e8c865022084accdf5602f33e0a26
Brandon Groves <brandon.groves@gmail.com>
2017-10-04 15:18:02 -0400

    Add rebase upstream/master for all projects

---
+++ gitscripts/rebaseall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for d in */ ; do
+  cd $d
+  if [ -d .git ]; then
+    git fetch upstream
+    # Don't rebase if no upstream exists
+    if [ $? -eq 0 ]; then
+      echo "\nrebase upstream/master for $d"
+      git checkout master
+      git rebase upstream/master
+    else
+      echo "\nno upstream for $d"
+    fi;
+  else
+    echo "\n$d is not a git repo"
+  fi;
+  cd ..
+done
--- shell.sh
+++ shell.sh
@@ -6,3 +6,4 @@ alias l='ls -CF'
 # projects and build aliases
 alias projects='cd ~/Projects'
 alias builds='cd ~/Builds'
+alias rball=`~/gitscripts/rebaseall.sh`
