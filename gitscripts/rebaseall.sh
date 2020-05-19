8e58d34589e54a359b74e18e2be5c9c4d945e9d6
Brandon Groves <brandon.groves@gmail.com>
2020-05-19 17:07:05 -0400

    Updated rebase all script to handle different branches

--- gitscripts/rebaseall.sh
+++ gitscripts/rebaseall.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+if [ $1 -eq 0 ]; then
+  echo "Please provide an argument to be used for the branch to rebase."
+fi;
+
+branchName=$1
+
 for d in */ ; do
   cd $d
   if [ -d .git ]; then
     git fetch upstream --prune
     # Don't rebase if no upstream exists
     if [ $? -eq 0 ]; then
-      echo "\nrebase upstream/master for $d"
-      git checkout master
-      git rebase upstream/master
+      echo -e "\nrebase upstream/$branchName for $d"
+      git checkout $branchName
+      git rebase upstream/$branchName
     else
-      echo "\nno upstream for $d"
+      echo -e "\nno upstream for $d"
     fi;
   else
-    echo "\n$d is not a git repo"
+    echo -e "\n$d is not a git repo"
   fi;
   cd ..
 done
