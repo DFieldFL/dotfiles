2dab9ec3a7210ce696e91417901bf5859b2d6ee2
Brandon Groves <brandon.groves@pm.me>
2024-06-26 08:04:36 -0400

    wezterm font size, nvim line numbers, and tmux left status

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('CodeNewRoman Nerd Font Mono')
+config.font_size = 13.0
 
 config.default_prog = { '/opt/homebrew/bin/tmux', 'new',  '-A', '-s', 'init' }
 
