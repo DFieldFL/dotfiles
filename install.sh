3b634164cd65d9c4665d962c11b30bbd8763516e
Brandon Groves <brandon.groves@pm.me>
2024-07-17 21:37:45 -0400

    Add linking to dotfiles

--- install.sh
+++ install.sh
@@ -8,13 +8,22 @@ if [ ! -d /opt/homebrew ] && [ ! -d /home/linuxbrew ]; then
     # echo "sourcing linux"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   fi
-
-  brew update
-  brew install wget git zsh zinit tmux tpm zoxide eza bat fzf btop tig jesseduffield/lazygit/lazygit k9s neovim diff-so-fancy jandedobbeleer/oh-my-posh/oh-my-posh
-
-  echo "Defaulting shell to zshell"
-  sudo chsh -s $(which zsh) $(whoami)
-
-  echo "To complete installation by restarting the terminal with zshell..."
-  echo "Load tmux using command 'tmx' and 'prefix + I' to install plugins..."
 fi
+
+brew update
+brew install wget git zsh zinit tmux tpm zoxide eza bat fzf btop tig jesseduffield/lazygit/lazygit k9s neovim diff-so-fancy jandedobbeleer/oh-my-posh/oh-my-posh
+
+echo "Defaulting shell to zshell"
+sudo chsh -s $(which zsh) $(whoami)
+
+echo "Setting up dotfiles"
+if [ -d ~/.confg ]; then
+  rm -rf ~/.config
+fi
+
+ln -sf "$(dirname "$0")/.config" ~/
+ln -sf "$(dirname "$0")/.zshrc" ~/.zshrc
+ln -sf "$(dirname "$0")/.tigrc" ~/.tigrc
+
+echo "To complete installation by restarting the terminal with zshell..."
+echo "Load tmux using command 'tmx' and 'prefix + I' to install plugins..."
