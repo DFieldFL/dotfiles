53a6356246812c49cc011b3be9b16bc8b06e0795
Brandon Groves <brandon.groves@pm.me>
2024-08-10 16:14:05 -0400

    install branch for a debian devcontainer

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
-#!/bin/bash
+sudo apt update
+sudo apt install -y curl git zsh tmux bat fzf btop tig zoxide
 
-if [ ! -d /opt/homebrew ] && [ ! -d /home/linuxbrew ]; then
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-if [ "$(uname)" == "Darwin" ]; then
-  # echo "sourcing mac"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-elif [ "$(uname)" == "Linux" ]; then
-  # echo "sourcing linux"
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
+curl -LOs https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+# ~/.zshrc sets the PATH for nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-brew update
-brew install wget git zsh zinit tmux tpm zoxide eza bat fzf btop tig jesseduffield/lazygit/lazygit k9s neovim diff-so-fancy jandedobbeleer/oh-my-posh/oh-my-posh
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Los lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+curl -LOs https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.4/diff-so-fancy
+chmod +x diff-so-fancy
+mv diff-so-fancy $HOME/.local/bin/
+
+sudo curl -s https://ohmyposh.dev/install.sh | bash -s
+
+sudo apt install -y gpg
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
 
 echo "Defaulting shell to zshell"
 sudo chsh -s $(which zsh) $(whoami)
@@ -27,18 +39,12 @@ if [ -f ~/.zshrc ]; then
   echo "Backing up ~/.zshrc to ~/.zshrc.bak"
   cp ~/.zshrc ~/.zshrc.bak
 fi
-if [ -f ~/.gitconfig ]; then
-  echo "Backing up ~/.gitconfig to ~/.gitconfig.bak"
-  cp ~/.gitconfig ~/.gitconfig.bak
-fi
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 cp -rf "$SCRIPT_DIR/.config" ~/
 cp -f "$SCRIPT_DIR/.zshrc" ~/.zshrc
-cp -f "$SCRIPT_DIR/.gitconfig" ~/.gitconfig
 cp -f "$SCRIPT_DIR/.tigrc" ~/.tigrc
 
 echo "To complete installation by restarting the terminal with zshell..."
-echo "Edit .gitconfig to include your email"
 echo "Load tmux using command 'tmx' and 'prefix + I' to install plugins..."
